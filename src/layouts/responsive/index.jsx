89ba7c24c4b162e37398f5e2c7f50d4c31dfda6f
sarahveiseh <sarahveiseh@yahoo.com>
2022-06-16 23:54:05 +0430

    fix header alignment

--- src/layouts/responsive/index.jsx
+++ src/layouts/responsive/index.jsx
@@ -54,14 +54,14 @@ export const ResponsiveLayout = (props) => {
           ))}
         </div>
         <div className="relative flex w-full h-full col-span-11 ">
-          <div className="fixed top-0 left-0 z-10 flex items-center justify-between w-full px-4 bg-white shadow h-14">
+          <div className="absolute top-0 left-0 z-10 flex items-center justify-between w-full px-4 bg-white shadow h-14">
             <span className="w-10">
               <ICBack className="w-5 h-5" />
             </span>
             <span className="text-center">مدلینک</span>
             <span className="w-10" />
           </div>
-          <div className="w-full overflow-y-scroll pt-14 bg-gray-50">
+          <div className="w-full overflow-y-scroll px-14 bg-gray-50">
             {props.children}
           </div>
         </div>
