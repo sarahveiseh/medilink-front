6a8d057005ab3858846c6fe002dff2fee14acd0e
Farshid Shahmoradi <farshidshahmoradi1996@gmail.com>
2022-06-24 18:58:56 +0430

    fix get all doctor

--- src/views/doctors/list/containers/doctors-container.jsx
+++ src/views/doctors/list/containers/doctors-container.jsx
@@ -1,28 +1,24 @@
-import { useGetPatientDoctors } from "services";
+import { useGetAllDoctors } from "services";
 
 export const DoctorsContainer = () => {
-  const { isLoading, isError, data } = useGetPatientDoctors();
+  const { isLoading, isError, data } = useGetAllDoctors();
+
+  //TODO : handle isLoading isError
 
   return (
-    <div className="w-full h-[80vh] grid grid-cols-1 border border-gray-50 sm:grid-cols-2 md:grid-cols-3 gap-5">
-      {[
-        { name: "دکی", age: 43, specialty: "کودکان" },
-        { name: "مدی", age: 56, specialty: "قلب" },
-        { name: "دکی", age: 43, specialty: "کودکان" },
-        { name: "مدی", age: 56, specialty: "قلب" },
-        { name: "دکی", age: 43, specialty: "کودکان" },
-        { name: "مدی", age: 56, specialty: "قلب" },
-        { name: "دکی", age: 43, specialty: "کودکان" },
-        { name: "مدی", age: 56, specialty: "قلب" },
-      ].map((doctor) => (
-        <div className="flex w-full gap-5 px-5 py-10 border border-gray-200 rounded hover:shadow-md hover:cursor-pointer">
+    <div className="w-full p-2 grid grid-cols-1  bg-white sm:grid-cols-2 md:grid-cols-3 gap-5">
+      {data?.result?.map((doctor) => (
+        <div className="duration-150 flex w-full gap-5 px-5 py-10 border border-gray-200 rounded hover:shadow-md hover:cursor-pointer">
           <div className="w-20 h-20 bg-blue-300 rounded-full shadow-sm ">
             آواتار
           </div>
           <div className="flex flex-col space-y-2 ">
-            <div>نام: {doctor.name}</div>
-            <div>سن: {doctor.age}</div>
-            <div>تخصص: {doctor.specialty}</div>
+            <div>
+              نام: {doctor?.firstName} {doctor?.lastName}
+            </div>
+            <div className="flex items-center">
+              تاریخ تولد: <p>{doctor.dateOfBirth}</p>
+            </div>
           </div>
         </div>
       ))}
